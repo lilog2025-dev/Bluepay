9d9d2ac681b7c1efdd2c30feb0d6bf7244a09198
lilog2025-dev <lilog2025@gmail.com>
2026-09-07 22:15:28 +0100

    Update page.tsx

--- app/electricity/page.tsx
+++ app/electricity/page.tsx
@@ -280,7 +280,7 @@ export default function ElectricityPage() {
             </div>
             <button
               type="button"
-              onClick={() => router.push('/buy-PayFlexCode')}
+              onClick={() => router.push('/buy-payflex-code')}
               className="text-[#0000ff] hover:text-blue-700 text-sm font-semibold mt-2"
             >
               Buy PayFlexCode
